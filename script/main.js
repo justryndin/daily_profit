541d513e5be0b8738ded07b43a2001fc627b0d03
Ivan <83250492+justryndin@users.noreply.github.com>
2025-02-20 17:29:30 +0300

    Add files via upload
    Добавление файлов на git

---
+++ script/main.js
@@ -0,0 +1,43 @@
+var app = new Vue ({
+    el:'#app',
+    data:{
+        menu: {
+            open: false
+        },
+        tab: {
+            active: 1,
+            top: 41
+        }
+    },
+    methods:{
+        toggleActive: function (e) {
+            e.target.classList.toggle('active');
+        },
+        scrollTo: function (e) {
+            var path = $(e.target).attr('href');
+            $('body, html').animate({
+                scrollTop: $(path).offset().top
+            },600);
+        },
+        menuOpen: function (e) {
+            this.menu.open = !this.menu.open
+        },
+        setTab: function (n, e) {
+            var self = e.target;
+            this.tab.active = n;
+            this.tab.top = self.offsetTop;
+        }
+    }
+});
+
+$('.s6').slick({
+    dots: true,
+    adaptiveHeight: true,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    pauseOnFocus: false,
+    pauseOnHover: false
+});
+$(window).on('scroll', function () {
+    app.menu.open = false;
+});
